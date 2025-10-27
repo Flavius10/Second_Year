3cf56efeb652436225b24775efb223a0a02198f7
Flavius10 <flaviusa1010@gmail.com>
2025-10-27 11:12:25 +0200

    Complete the Duck Class

--- Metode_avansate_de_programare/DuckSocialNetwork/DuckSocialNetwork/src/main/java/org/example/domain/Duck.java
+++ Metode_avansate_de_programare/DuckSocialNetwork/DuckSocialNetwork/src/main/java/org/example/domain/Duck.java
@@ -16,4 +16,65 @@ public class Duck extends User{
         this.card = card;
     }
 
+    /// Getters
+    public TypeDuck getTip() {
+        return this.tip;
+    }
+
+    public double getViteza() {
+        return this.viteza;
+    }
+
+    public double getRezistenta() {
+        return this.rezistenta;
+    }
+
+    public Card getCard() {
+        return this.card;
+    }
+
+    /// Setters
+    public void setTip(TypeDuck tip) {
+        this.tip = tip;
+    }
+
+    public void setViteza(double viteza) {
+        this.viteza = viteza;
+    }
+
+    public void setRezistenta(double rezistenta) {
+        this.rezistenta = rezistenta;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    @Override
+    public String toString() {
+        return "Duck{" +
+                "tip=" + tip +
+                ", viteza=" + viteza +
+                ", rezistenta=" + rezistenta +
+                ", card=" + card +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Duck other = (Duck) obj;
+        return other.getId().equals(this.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getId().hashCode();
+    }
+
 }
