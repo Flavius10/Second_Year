fd7f6ac877702e68178c169a0de740364a8e1a11
Flavius10 <flaviusa1010@gmail.com>
2025-10-28 10:02:27 +0200

    Implement the DuckService

---
+++ Metode_avansate_de_programare/DuckSocialNetwork/DuckSocialNetwork/src/main/java/org/example/services/DuckService.java
@@ -0,0 +1,54 @@
+package org.example.services;
+
+import org.example.domain.User;
+import org.example.exceptions.UserAlreadyExists;
+import org.example.exceptions.UserNotFound;
+import org.example.repositories.RepoFileDuck;
+import org.example.repositories.RepoFilePersoana;
+
+public class DuckService {
+
+    private RepoFileDuck repoFileDuck;
+
+    public DuckService(RepoFileDuck repoFileDuck){
+        this.repoFileDuck = repoFileDuck;
+    }
+
+    public void saveDuck(User user, String file_name){
+
+        try {
+            this.repoFileDuck.save(user, file_name);
+        } catch (UserAlreadyExists e) {
+            throw new UserAlreadyExists(e.toString());
+        }
+    }
+
+    public void deleteDuck(User user, String file_name){
+        try{
+            this.repoFileDuck.delete(user, file_name);
+        } catch(UserNotFound e){
+            throw new UserNotFound(e.toString());
+        }
+    }
+
+    public void updateDuck(User user, String file_name){
+        try{
+            this.repoFileDuck.update(user, file_name);
+        } catch(UserNotFound e){
+            throw new UserNotFound(e.toString());
+        }
+    }
+
+    public User findByIdDuck(Long id, String file_name){
+        return this.repoFileDuck.findById(id, file_name);
+    }
+
+    public Iterable<User> findAllDucks(String file_name){
+        return this.repoFileDuck.findAll(file_name);
+    }
+
+    public User findByUsernameDuck(String username, String file_name){
+        return this.repoFileDuck.findByUsername(username, file_name);
+    }
+
+}
