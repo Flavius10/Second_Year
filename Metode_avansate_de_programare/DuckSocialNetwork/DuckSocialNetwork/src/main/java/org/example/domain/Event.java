3f008f6f96c981b9d96e25c6859b7c33b2245233
Flavius10 <flaviusa1010@gmail.com>
2025-11-04 18:23:11 +0200

    Implement the Event class

---
+++ Metode_avansate_de_programare/DuckSocialNetwork/DuckSocialNetwork/src/main/java/org/example/domain/Event.java
@@ -0,0 +1,36 @@
+package org.example.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event {
+
+    private List<User> subscribers;
+
+    public Event() {
+        this.subscribers = new ArrayList<>();
+    }
+
+    public void subscribe(User user){
+        if (!subscribers.contains(user))
+        {
+            subscribers.add(user);
+            System.out.println("User " + user.getUsername() + " subscribed to event");
+        }
+    }
+
+    public void unsubscribe(User user){
+        boolean removed = subscribers.remove(user);
+
+        if (removed) {
+            System.out.println("User " + user.getUsername() + " unsubscribed from event");
+        } else {
+            System.out.println("User " + user.getUsername() + " was not subscribed to event");
+        }
+    }
+
+    public void notifySubscribers(){
+        System.out.println("Notifying subscribers...");
+    }
+
+}
