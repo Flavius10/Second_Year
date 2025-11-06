2547ac9a29a76dc6cc297940b179e69e71268c2b
Flavius10 <flaviusa1010@gmail.com>
2025-11-06 09:04:20 +0200

    Implement the idea of the RaceService

--- Metode_avansate_de_programare/DuckSocialNetwork/DuckSocialNetwork/src/main/java/org/example/domain/events/RaceService.java
+++ Metode_avansate_de_programare/DuckSocialNetwork/DuckSocialNetwork/src/main/java/org/example/domain/events/RaceService.java
@@ -11,12 +11,22 @@ public class RaceService{
 
     private FeasibilityChecker checker;
     private DuckSelector selector;
+    private List<SwimmingDuck> sortedDucks;
+    private List<Lane> lanes;
+
+    public RaceService(FeasibilityChecker checker, DuckSelector selector,
+                       List<SwimmingDuck> sortedDucks, List<Lane> lanes) {
+        this.checker = checker;
+        this.selector = selector;
+        this.sortedDucks = sortedDucks;
+        this.lanes = lanes;
+    }
 
 
-    public boolean runRace(List<SwimmingDuck> sortedDucks, List<Lane> lanes, double time){
+    public boolean runRace(double time){
 
         RaceEvaluator evaluator = this.checker.getRaceEvaluator();
-        
+
         List<SwimmingDuck> ducks = this.selector.selectDucks(sortedDucks, lanes, lanes.size(), time, evaluator);
 
         if (ducks == null) return false;
