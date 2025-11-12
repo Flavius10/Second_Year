c138d6380ed832786e6296151386651946c7882d
Flavius10 <flaviusa1010@gmail.com>
2025-11-12 09:34:53 +0200

    Make the lab for the Probabilistics and Statistics

--- Metode_avansate_de_programare/DuckSocialNetwork/DuckSocialNetwork/src/main/java/org/example/domain/ducks/Duck.java
+++ Metode_avansate_de_programare/DuckSocialNetwork/DuckSocialNetwork/src/main/java/org/example/domain/ducks/Duck.java
@@ -1,133 +1,133 @@
-package org.example.domain.ducks;
+    package org.example.domain.ducks;
 
-import org.example.domain.TypeDuck;
-import org.example.domain.User;
-import org.example.domain.ducks.card.Card;
-
-/**
- * The type Duck.
- */
-public abstract class Duck extends User {
-
-    private TypeDuck tip;
-    private double viteza;
-    private double rezistenta;
-    private Card card;
+    import org.example.domain.TypeDuck;
+    import org.example.domain.User;
+    import org.example.domain.ducks.card.Card;
 
     /**
-     * Instantiates a new Duck.
-     *
-     * @param id         the id
-     * @param name       the name
-     * @param email      the email
-     * @param password   the password
-     * @param tip        the tip
-     * @param viteza     the viteza
-     * @param rezistenta the rezistenta
-     * @param card       the card
+     * The type Duck.
      */
-    public Duck(Long id, String name, String email, String password,
-                TypeDuck tip, double viteza, double rezistenta, Card card){
-        super(id, name, email, password);
-        this.tip = tip;
-        this.viteza = viteza;
-        this.rezistenta = rezistenta;
-        this.card = card;
-    }
+    public abstract class Duck extends User {
 
-    /**
-     * Getters @return  the tip
-     */
-    public TypeDuck getTip() {
-        return this.tip;
-    }
+        private TypeDuck tip;
+        private double viteza;
+        private double rezistenta;
+        private Card card;
 
-    /**
-     * Gets viteza.
-     *
-     * @return the viteza
-     */
-    public double getViteza() {
-        return this.viteza;
-    }
+        /**
+         * Instantiates a new Duck.
+         *
+         * @param id         the id
+         * @param name       the name
+         * @param email      the email
+         * @param password   the password
+         * @param tip        the tip
+         * @param viteza     the viteza
+         * @param rezistenta the rezistenta
+         * @param card       the card
+         */
+        public Duck(Long id, String name, String email, String password,
+                    TypeDuck tip, double viteza, double rezistenta, Card card){
+            super(id, name, email, password);
+            this.tip = tip;
+            this.viteza = viteza;
+            this.rezistenta = rezistenta;
+            this.card = card;
+        }
 
-    /**
-     * Gets rezistenta.
-     *
-     * @return the rezistenta
-     */
-    public double getRezistenta() {
-        return this.rezistenta;
-    }
+        /**
+         * Getters @return  the tip
+         */
+        public TypeDuck getTip() {
+            return this.tip;
+        }
 
-    /**
-     * Gets card.
-     *
-     * @return the card
-     */
-    public Card getCard() {
-        return this.card;
-    }
+        /**
+         * Gets viteza.
+         *
+         * @return the viteza
+         */
+        public double getViteza() {
+            return this.viteza;
+        }
 
-    /**
-     * Setters @param tip the tip
-     */
-    public void setTip(TypeDuck tip) {
-        this.tip = tip;
-    }
+        /**
+         * Gets rezistenta.
+         *
+         * @return the rezistenta
+         */
+        public double getRezistenta() {
+            return this.rezistenta;
+        }
 
-    /**
-     * Sets viteza.
-     *
-     * @param viteza the viteza
-     */
-    public void setViteza(double viteza) {
-        this.viteza = viteza;
-    }
+        /**
+         * Gets card.
+         *
+         * @return the card
+         */
+        public Card getCard() {
+            return this.card;
+        }
 
-    /**
-     * Sets rezistenta.
-     *
-     * @param rezistenta the rezistenta
-     */
-    public void setRezistenta(double rezistenta) {
-        this.rezistenta = rezistenta;
-    }
+        /**
+         * Setters @param tip the tip
+         */
+        public void setTip(TypeDuck tip) {
+            this.tip = tip;
+        }
 
-    /**
-     * Sets card.
-     *
-     * @param card the card
-     */
-    public void setCard(Card card) {
-        this.card = card;
-    }
+        /**
+         * Sets viteza.
+         *
+         * @param viteza the viteza
+         */
+        public void setViteza(double viteza) {
+            this.viteza = viteza;
+        }
 
-    @Override
-    public String toString() {
-        return "Duck{" +
-                "tip=" + tip +
-                ", viteza=" + viteza +
-                ", rezistenta=" + rezistenta +
-                ", card=" + card +
-                '}';
-    }
+        /**
+         * Sets rezistenta.
+         *
+         * @param rezistenta the rezistenta
+         */
+        public void setRezistenta(double rezistenta) {
+            this.rezistenta = rezistenta;
+        }
 
-    @Override
-    public boolean equals(Object obj){
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Duck other = (Duck) obj;
-        return other.getId().equals(this.getId());
-    }
+        /**
+         * Sets card.
+         *
+         * @param card the card
+         */
+        public void setCard(Card card) {
+            this.card = card;
+        }
 
-    @Override
-    public int hashCode() {
-        return this.getId().hashCode();
-    }
+        @Override
+        public String toString() {
+            return "Duck{" +
+                    "tip=" + tip +
+                    ", viteza=" + viteza +
+                    ", rezistenta=" + rezistenta +
+                    ", card=" + card +
+                    '}';
+        }
 
-}
+        @Override
+        public boolean equals(Object obj){
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Duck other = (Duck) obj;
+            return other.getId().equals(this.getId());
+        }
+
+        @Override
+        public int hashCode() {
+            return this.getId().hashCode();
+        }
+
+    }
---
+++ Metode_avansate_de_programare/DuckSocialNetwork/DuckSocialNetwork/src/main/java/org/example/repositories/repo_db/ReopDB.java
@@ -0,0 +1,6 @@
+package org.example.repositories.repo_db;
+
+public interface ReopDB {
+
+
+}
